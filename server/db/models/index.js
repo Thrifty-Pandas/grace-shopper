2d1eca4386977ea8ae1f9269133edc8c1fb9dc23
lgrees <lukasgrees@gmail.com>
2018-10-30 11:22:16 -0500

    adds product model and category association

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,7 @@
 const User = require('./user')
+const Product = require('./product')
+
+Product.belongsToMany(User, {through: 'ProductCategories'})
 
 /**
  * If we had any associations to make, this would be a great place to put them!
