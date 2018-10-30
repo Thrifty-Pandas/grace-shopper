ec4d99a0addcfbbd043763bd3aa33d19283c449e
lgrees <lukasgrees@gmail.com>
2018-10-30 11:34:53 -0500

    adds naive all products route without eager loading

---
+++ server/api/products.js
@@ -0,0 +1,11 @@
+const {Product} = require('../db/models/product')
+const router = require('express').Router()
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
