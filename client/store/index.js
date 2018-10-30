84e3f2de4c6dff61cf29b888e88e15e74c068f44
Patrick <pdylanconroy@gmail.com>
2018-10-30 17:12:22 -0500

    typo in store index fixed: user --> users

--- client/store/index.js
+++ client/store/index.js
@@ -3,7 +3,7 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
-import {usersReducer} from "./user"
+import {usersReducer} from "./users"
 import {productsReducer} from './products'
 
 const reducer = combineReducers({user, products: productsReducer, users: usersReducer})
