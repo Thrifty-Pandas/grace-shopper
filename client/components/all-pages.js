6d0dfa08cf4ff9ac0c2c5229bb15776a6d1f4ae9
lgrees <lukasgrees@gmail.com>
2018-10-30 17:05:15 -0500

    adds elementary all products page

---
+++ client/components/all-pages.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchProducts} from '../store/products'
+
+//requires store
+const mapState = ({products}) => ({products})
+
+//requires thunk
+
+const mapProps = {fetchProducts}
+
+export class AllPages extends Component {
+  componentDidMount() {
+    this.props.fetchProducts()
+  }
+
+  render() {
+    const {products} = this.props
+    return (
+      <div>
+        <ul>
+          {products.map(product => <li key={product.id}>{product.name}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default AllPages
