1b38963e2ae06a6eb785ac27452080eefddbafc0
lgrees <lukasgrees@gmail.com>
2018-10-30 11:43:43 -0500

    adds post and find single product

--- server/api/products.js
+++ server/api/products.js
@@ -9,3 +9,22 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findById(Number(req.params.productId))
+    res.json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  const {name, description, stock, price} = req.body
+  try {
+    const product = await Product.create({name, description, stock, price})
+    res.json(product)
+  } catch (err) {
+    next(err)
+  }
+})
