a170cdf2d765d3182f338d6758ded6d11f8e3171
lgrees <lukasgrees@gmail.com>
2018-10-30 20:06:15 -0500

    minor formatting

--- client/components/all-pages.js
+++ client/components/all-pages.js
@@ -2,10 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchProducts} from '../store/products'
 
-//requires store
 const mapState = ({products}) => ({products})
-
-//requires thunk
 
 const mapDispatch = {fetchProducts}
 
