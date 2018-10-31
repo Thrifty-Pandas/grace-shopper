1b84b628cd72a92cf7d7c60d412c4ac577f93181
Lotus T <ltan93@gmail.com>
2018-10-31 16:13:02 -0500

    added more validation on reviews model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -9,8 +9,14 @@ const ProductCategory = db.define('productCategory')
 Category.belongsToMany(Product, {through: ProductCategory})
 Product.belongsToMany(Category, {through: ProductCategory})
 
-Reviews.belongsTo(Product)
-Reviews.belongsTo(User)
+Reviews.belongsTo(Product, {
+  onDelete: 'CASCADE',
+  foreignKey: {name: 'product_id', allowNull: false}
+})
+Reviews.belongsTo(User, {
+  onDelete: 'CASCADE',
+  foreignKey: {name: 'user_id', allowNull: false}
+})
 
 // including the association on both models instead of just on Reviews so that we can access Sequelize Magic Methods on either model
 
