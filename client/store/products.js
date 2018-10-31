679b4ed04d5dd594bd96c5179e11001b70f90855
Julia Lee <hopefullyea@gmail.com>
2018-10-31 10:12:05 -0500

    Single product store added

--- client/store/products.js
+++ client/store/products.js
@@ -1,12 +1,19 @@
-export const GET_PRODUCTS = 'GET_PRODUCTS'
-export const ADD_PRODUCT = 'ADD_PRODUCT'
 import axios from 'axios'
 
 const initialState = []
 
+export const GET_PRODUCTS = 'GET_PRODUCTS'
+export const GET_ONEPRODUCT = 'GET_ONEPRODUCT'
+export const ADD_PRODUCT = 'ADD_PRODUCT'
+
 export const getProducts = products => ({
   type: GET_PRODUCTS,
   products
+})
+
+export const getOneProduct = product => ({
+  type: GET_ONEPRODUCT,
+  product
 })
 
 export const fetchProducts = () => async dispatch => {
@@ -18,10 +25,21 @@ export const fetchProducts = () => async dispatch => {
   }
 }
 
+export const fetchOneProduct = product => async dispatch => {
+  try {
+    const {data} = await axios.get(`/api/products/${product.id}`)
+    dispatch(getOneProduct(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 export const productsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
       return action.products
+    case GET_ONEPRODUCT:
+      return action.product
     default:
       return state
   }
