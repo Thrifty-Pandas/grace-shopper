c95a5fd6884d5cc6e7dc79cb69d8947966f16c93
Patrick <pdylanconroy@gmail.com>
2018-10-31 13:46:49 -0500

    initial commit

--- client/store/index.js
+++ client/store/index.js
@@ -8,7 +8,7 @@ import {productsReducer} from './products'
 import {categoriesReducer} from './categories'
 
 import {filterReducer} from './filter'
-
+import {searchReducer} from './search'
 
 const reducer = combineReducers({
   user,
@@ -16,8 +16,9 @@ const reducer = combineReducers({
 
   categories: categoriesReducer,
 
-  filter: filterReducer
+  filter: filterReducer,
 
+  search: searchReducer
 })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
