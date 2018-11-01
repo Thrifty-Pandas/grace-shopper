563c2c06f9592f459443eb0e7435ca63fa75da5a
Lotus T <ltan93@gmail.com>
2018-11-01 11:15:45 -0500

    fixed issue with UserId not actually being a foreign key

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -2,10 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Cart = db.define('cart', {
-  userId: {
-    type: Sequelize.INTEGER,
-    allowNull: true
-  },
   temporaryUserId: {
     type: Sequelize.INTEGER,
     allowNull: true
