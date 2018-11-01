4caaafdd2550a6c6ac02e8cad8ae9ca8e6cb2cc3
lgrees <lukasgrees@gmail.com>
2018-11-01 17:30:54 -0500

    fixes single product render bug

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -4,50 +4,41 @@ import {Container, Grid, Card, Divider, Image} from 'semantic-ui-react'
 import {fetchOneProduct} from '../store/products'
 
 class SingleProduct extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      product: this.props.oneProduct
-    }
-  }
-
   componentDidMount() {
     const productId = Number(this.props.match.params.productId)
     this.props.fetchProduct(productId)
   }
 
   render() {
-    const product = this.state.product
+    const product = this.props.products.selectedProduct
     return (
-      <Grid columns={2} relaxed>
-        <Grid.Column>
-          <Card>
-            <Image src={product.imageUrl} />
-            <Card.Content>
-              <Card.Header> Product{product.name}</Card.Header>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-        <Divider vertical hidden />
-        <Grid.Column>
-          <Grid columns={2} relaxed>
-            <Grid.Column>{product.price}</Grid.Column>
-            <Grid.Column>{product.stock}</Grid.Column>
-          </Grid>
-          <Container>
-            <p> {product.description} </p>
-          </Container>
-        </Grid.Column>
-      </Grid>
+      product.id && (
+        <Grid columns={2} relaxed>
+          <Grid.Column>
+            <Card>
+              <Image src={product.imageUrl} />
+              <Card.Content>
+                <Card.Header> Product{product.name}</Card.Header>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+          <Divider vertical hidden />
+          <Grid.Column>
+            <Grid columns={2} relaxed>
+              <Grid.Column>{product.price}</Grid.Column>
+              <Grid.Column>{product.stock}</Grid.Column>
+            </Grid>
+            <Container>
+              <p> {product.description} </p>
+            </Container>
+          </Grid.Column>
+        </Grid>
+      )
     )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    oneProduct: state.products.selectedProduct
-  }
-}
+const mapStateToProps = ({products}) => ({products})
 
 const mapDispatchToProps = dispatch => {
   return {
