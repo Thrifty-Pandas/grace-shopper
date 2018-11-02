6ce45d0264303f6bfe69a4c7b373cd311a72650b
Lotus T <ltan93@gmail.com>
2018-11-02 12:17:12 -0500

    update product in cart api route

--- server/api/cart.js
+++ server/api/cart.js
@@ -3,13 +3,10 @@ const {User, Cart, Product, CartProduct} = require('../db/models')
 
 async function findCart(sessionInfo) {
   let existingUser = {}
-  // Check if user is authenticated or guest:
   if (sessionInfo.user) {
-    console.log('hi', sessionInfo.user.id)
-    existingUser = await User.findById(sessionInfo.userId) // assumes that authenticated user info will be persisted using req.session.userId, adjust and verify when user authentication is completed
+    existingUser = await User.findById(sessionInfo.userId)
   }
   let cart = {}
-  // Create or find existing cart on Cart model:
   if (!existingUser.id) {
     const cartinstance = await Cart.findOrCreate({
       where: {
@@ -21,7 +18,6 @@ async function findCart(sessionInfo) {
       : sessionInfo.id
     cart = cartinstance[0]
   } else {
-    console.log('existing user')
     const cartinstance = await Cart.findOrCreate({
       where: {userId: existingUser.id}
     })
@@ -33,19 +29,34 @@ async function findCart(sessionInfo) {
 router.post('/:productId', async (req, res, next) => {
   try {
     const cart = await findCart(req.session)
-    console.log(cart)
-    // Find product
     const product = await Product.findById(req.params.productId)
-    console.log('cart id: ', cart.id)
-    console.log('product id: ', product.id)
-    console.log('quantity: ', req.body.quantity)
-    // Take cart and product and create instance on CartProduct with foreign keys referring to Cart model and Product model
     const newProductInCart = await CartProduct.create({
       cartId: cart.id,
       productId: product.id,
       quantity: req.body.quantity ? req.body.quantity : 1
     })
     res.status(201).json(newProductInCart)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const cart = await findCart(req.session)
+    const product = await Product.findById(req.params.productId)
+    const currentProductInCart = await CartProduct.find({
+      where: {
+        cartId: cart.id,
+        productId: product.id
+      }
+    })
+    const updatedProduct = await currentProductInCart.update({
+      quantity:
+        currentProductInCart.quantity +
+        (req.body.quantity ? req.body.quantity : 1)
+    })
+    res.status(204).json(updatedProduct)
   } catch (err) {
     next(err)
   }
