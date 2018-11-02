7eb5596e4904154a99974f357de7c5c2b329d02d
Julia Lee <40521725+JuliaSLee@users.noreply.github.com>
2018-11-02 16:07:34 -0500

    Update orders.js

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,33 +1,67 @@
-const {Order} = require('../db/models')
+const {Order, OrderProduct} = require('../db/models')
 const router = require('express').Router()
+
 module.exports = router
 
+async function findOrders(sessionInfo) {
+  if (sessionInfo.temporaryUserId) {
+    const orders = await Order.findAll({
+      where: {
+        temporaryUserId: sessionInfo.temporaryUserId
+      }
+    })
+    return orders
+  } else {
+    const orders = await Order.findAll({
+      where: {
+        userId: sessionInfo.userId
+      }
+    })
+    return orders
+  }
+}
+
+// shows all the orders for a specific user
 router.get('/', async (req, res, next) => {
   try {
-    const orders = await Order.findAll()
-    res.json(orders)
+    const orders = await findOrders(req.session)
+    res.status(200).json(orders)
   } catch (err) {
     next(err)
   }
 })
 
+// shows one specific order for a specific user
 router.get('/:orderId', async (req, res, next) => {
   try {
-    const order = await Order.findById(Number(req.params.orderId))
-    res.json(order)
+    const productsInOrder = await OrderProduct.findAll({
+      where: {
+        orderId: req.params.id
+      }
+    })
+    res.status(200).json(productsInOrder)
   } catch (err) {
     next(err)
   }
 })
 
+// addd new order to a specific user
 router.post('/', async (req, res, next) => {
-  const {shippingAddress, email, price, temporaryUserId, status} = req.body
+  const {
+    shippingAddress,
+    email,
+    price,
+    temporaryUserId,
+    status,
+    userId
+  } = req.body
   try {
     const order = await Order.create({
       shippingAddress,
       email,
       price,
       temporaryUserId,
+      userId,
       status
     })
     res.json(order)
