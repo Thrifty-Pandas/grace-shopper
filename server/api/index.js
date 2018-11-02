2602720f8d66f282eb86b196f4d547b65d8646e4
Julia Lee <40521725+JuliaSLee@users.noreply.github.com>
2018-11-02 14:40:58 -0500

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -4,11 +4,8 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/products', require('./products'))
 router.use('/categories', require('./categories'))
-<<<<<<< HEAD
 router.use('/orders', require('./orders'))
-=======
 router.use('/cart', require('./cart'))
->>>>>>> a90da65f8263a5e985cacbe8685d3b0329b14dd9
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
