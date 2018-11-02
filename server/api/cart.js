96e56db403c0af5284b01dce4413815a570e7928
Lotus T <ltan93@gmail.com>
2018-11-01 21:22:58 -0500

    hackily functional persistent cart, need to fix

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,21 +4,25 @@ const {User, Cart, Product, CartProduct} = require('../db/models')
 router.post('/:productId', async (req, res, next) => {
   try {
     let existingUser = {}
-
     // Check if user is authenticated or guest:
-    if (req.session.userId) {
+    if (req.session.user) {
+      console.log('hi', req.session.user.id)
       existingUser = await User.findById(req.session.userId) // assumes that authenticated user info will be persisted using req.session.userId, adjust and verify when user authentication is completed
     }
     let cart = {}
     // Create or find existing cart on Cart model:
-    if (!existingUser) {
-      const cartinstance = Cart.findOrCreate({
+    if (!existingUser.id) {
+      const cartinstance = await Cart.findOrCreate({
         where: {
-          temporaryUserId: req.session.id // TODO: needs to be encrypted to uuid but using it directly for now
+          temporaryUserId: req.session.id
         }
       })
+      req.session.temporaryUserId = req.session.temporaryUserId
+        ? req.session.temporaryUserId
+        : req.session.id
       cart = cartinstance[0]
     } else {
+      console.log('existing user')
       const cartinstance = await Cart.findOrCreate({
         where: {userId: existingUser.id}
       })
@@ -27,7 +31,9 @@ router.post('/:productId', async (req, res, next) => {
 
     // Find product:
     const product = await Product.findById(req.params.productId)
-
+    console.log('cart id: ', cart.id)
+    console.log('product id: ', product.id)
+    console.log('quantity: ', req.body.quantity)
     // Take cart and product and create instance on CartProduct with foreign keys referring to Cart model and Product model
     const newProductInCart = await CartProduct.create({
       cartId: cart.id,
