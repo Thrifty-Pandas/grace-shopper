3f29a92b86d4f56dd049c3fc39296e43f3b7f0fb
lgrees <lukasgrees@gmail.com>
2018-11-05 10:57:49 -0600

    adds category form and restricts to admins

--- client/components/product-form.js
+++ client/components/product-form.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import {Form, Input} from 'semantic-ui-react'
+import {Form, Input, Icon} from 'semantic-ui-react'
+import {connect} from 'react-redux'
+
+//even though we might have this in an entirely separate page,to me it
+//makes sense to restrict access at the component level to have the
+//extra guarantee
+const mapState = ({user}) => ({user})
 
 const ProductForm = props => {
   const {
@@ -13,56 +19,60 @@ const ProductForm = props => {
     type
   } = props
   return (
-    <Form onSubmit={handleSubmit} size="big">
-      <Form.Group>
-        <Form.Field
-          label="Product Name"
-          control={Input}
-          placeholder="Product Name"
-          name="name"
-          value={name}
-          onChange={handleChange}
-        />
-        <Form.Field
-          label="Description"
-          control={Input}
-          placeholder="Description"
-          name="description"
-          value={description}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Field
-          label="Stock"
-          control={Input}
-          placeholder="Stock"
-          name="stock"
-          value={stock}
-          onChange={handleChange}
-        />
-        <Form.Field
-          label="Price"
-          control={Input}
-          placeholder="Price"
-          name="price"
-          value={price}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Field
-          label="Image URL"
-          control={Input}
-          placeholder="Image URL"
-          name="imageUrl"
-          value={imageUrl}
-          onChange={handleChange}
-        />
-        <Form.Button type="submit">{type} Product</Form.Button>
-      </Form.Group>
-    </Form>
+    props.user.isAdmin && (
+      <Form onSubmit={handleSubmit} size="big">
+        <Form.Group>
+          <Form.Field
+            label="Product Name"
+            control={Input}
+            placeholder="Product Name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+          />
+          <Form.Field
+            label="Description"
+            control={Input}
+            placeholder="Description"
+            name="description"
+            value={description}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Field
+            label="Stock"
+            control={Input}
+            placeholder="Stock"
+            name="stock"
+            value={stock}
+            onChange={handleChange}
+          />
+          <Form.Field
+            label="Price"
+            control={Input}
+            placeholder="Price"
+            name="price"
+            value={price}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Field
+            label="Image URL"
+            control={Input}
+            placeholder="Image URL"
+            name="imageUrl"
+            value={imageUrl}
+            onChange={handleChange}
+          />
+          <Form.Button type="submit">
+            <Icon name={type.toLowerCase()} /> {type} Product
+          </Form.Button>
+        </Form.Group>
+      </Form>
+    )
   )
 }
 
-export default ProductForm
+export default connect(mapState)(ProductForm)
