b0a1b6dee882a655ca91fbfec85236b2c80a3a34
lgrees <lukasgrees@gmail.com>
2018-11-05 10:41:15 -0600

    add category actions + api route

--- client/store/categories.js
+++ client/store/categories.js
@@ -3,11 +3,25 @@ import axios from 'axios'
 const initialState = []
 
 export const SET_CATEGORIES = 'SET_CATEGORIES'
+export const ADD_CATEGORY = 'ADD_CATEGORY'
 
 export const setCategories = categories => ({
   type: SET_CATEGORIES,
   categories
 })
+export const addCategory = category => ({
+  type: ADD_CATEGORY,
+  category
+})
+
+export const postCategory = category => async dispatch => {
+  try {
+    const {data} = await axios.post('/api/categories', category)
+    dispatch(addCategory(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 export const fetchCategories = () => async dispatch => {
   try {
@@ -22,6 +36,8 @@ export const categoriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_CATEGORIES:
       return action.categories
+    case ADD_CATEGORY:
+      return [...state, action.category]
     default:
       return state
   }
