a68b06bcae2699d34ca11b262fe33d1f69148fd5
lgrees <lukasgrees@gmail.com>
2018-11-06 10:50:19 -0600

    adds instance method to cart

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const {Order, OrderProduct, Product} = require('../db/models')
 
 const Cart = db.define('cart', {
   temporaryUserId: {
@@ -8,6 +9,19 @@ const Cart = db.define('cart', {
   }
 })
 
-Cart.prototype.createOrder = function(orderInfo) {}
+Cart.prototype.createOrder = async function(orderInfo) {
+  //create an order
+  const order = await Order.create({ ...orderInfo })
+  // const cart = await Cart.findById(this.id, { include: [Product] })
+
+  await Promise.all(
+
+    this.products.map(product =>
+      OrderProduct.create({productId:product.id,orderId:order.id,price:product.price,quantity:})
+      )
+
+  )
+  //create orderProducts
+}
 
 module.exports = Cart
