4c55823ab8c2f69b8917e8e8d186a8dd621d4ac7
Julia Lee <hopefullyea@gmail.com>
2018-11-06 13:04:49 -0600

    filter done

--- client/components/all-orders.js
+++ client/components/all-orders.js
@@ -1,26 +1,26 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchOrders} from '../store'
-import {OrdersGrid, FilterForm} from './index'
+import {OrdersGrid, OrderFilterForm} from './index'
 
-const mapStateToProps = ({orders, filter}) => ({
+const mapStateToProps = ({orders, orderfilter}) => ({
   allOrders: orders.allOrders,
-  filter
+  orderfilter
 })
 
 const mapDispatchToProps = dispatch => ({
   fetchAllOrders: () => dispatch(fetchOrders())
 })
 
-const filterOrders = (allOrders, statusFilters) => {
-  if (statusFilters.length) {
-    allOrders.filter(order => {
-      statusFilters.map(statusFilter => {
-        if (statusFilter === order.status) {
-          return order.status
-        }
-      })
-    })
+const filterOrders = (allOrders, statusFilter) => {
+  if (statusFilter.length) {
+    // console.log(
+    //   'mapping, filtering',
+    //   statusFilter.map(status =>
+    //     allOrders.filter(order => status === order.status)
+    //   )
+    // )
+    return allOrders.filter(order => statusFilter.includes(order.status))
   } else {
     return allOrders
   }
@@ -32,12 +32,13 @@ class AllOrders extends Component {
   }
 
   render() {
-    const {allOrders, filter} = this.props
-    const ordersToDisplay = filterOrders(allOrders, filter)
+    const {allOrders, orderfilter} = this.props
+    const ordersToDisplay = filterOrders(allOrders, orderfilter)
+    console.log('ordersToDisplay->', ordersToDisplay)
     return (
       <div>
-        <FilterForm />
-        <OrdersGrid allOrders={ordersToDisplay} />
+        <OrderFilterForm />
+        <OrdersGrid ordersToDisplay={ordersToDisplay} />
       </div>
     )
   }
