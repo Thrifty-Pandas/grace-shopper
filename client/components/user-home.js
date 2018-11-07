2feb5154b3f5aa11a7db461e051ee1fdc7f99348
Julia Lee <hopefullyea@gmail.com>
2018-11-06 21:27:42 -0600

    minor changes

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -7,11 +7,11 @@ import {connect} from 'react-redux'
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  const {firstName} = props
 
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+      <h3>Welcome, {firstName}!</h3>
       <Link to="/orders">View Orders</Link>
     </div>
   )
@@ -22,7 +22,7 @@ export const UserHome = props => {
  */
 const mapState = state => {
   return {
-    email: state.user.email
+    firstName: state.user.firstName
   }
 }
 
